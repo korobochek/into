42ad5d2999d06bac423ba8a555040fb30f1c859a
Olga Goloshchapova <olga.goloshchapova@mable.com.au>
2026-02-06 07:49:53 +1100

    implementing "sizes" function - not quite happy with it, but it does the trick

--- src/factorhouse/kafka/topic.clj
+++ src/factorhouse/kafka/topic.clj
@@ -2,9 +2,21 @@
 
 ;; Technical Challenge! Implement this function.
 
+;; ASSUMPTION:
+;; given the statement in data file: 
+;; "* Each Broker having a single '/kfk' directory"
+;; there will be always just one directory and it will always be called '/kfk'
+(defn- format-topics [topics]
+  (mapcat
+   (fn [[broker, dir-data]]
+     (map #(conj (apply merge %) {:broker broker :dir "/kfk"})
+          (get-in dir-data ["/kfk" :replica-infos])))
+   topics))
+
 (defn sizes
   "Transform raw topic information into a more useable sizes shape"
-  [topics])
+  [topics] (vec (sort-by (juxt :broker :topic :partition) (format-topics topics))))
+
 
 ;; Extension Challenge! Implement these functions.
 
