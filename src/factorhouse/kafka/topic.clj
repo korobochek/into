df1d9558ee40ae6f92883cd1118a73b5d7defcef
Olga Goloshchapova <olga.goloshchapova@mable.com.au>
2026-02-16 19:31:42 +1100

    implement categories-logical method, again, not 100% happy with it

--- src/factorhouse/kafka/topic.clj
+++ src/factorhouse/kafka/topic.clj
@@ -1,4 +1,6 @@
-(ns factorhouse.kafka.topic)
+(ns factorhouse.kafka.topic
+  (:require
+   [factorhouse.kafka.topic :as topic]))
 
 ;; Technical Challenge! Implement this function.
 
@@ -17,13 +19,40 @@
   "Transform raw topic information into a more useable sizes shape"
   [topics] (vec (sort-by (juxt :broker :topic :partition) (format-topics topics))))
 
-
 ;; Extension Challenge! Implement these functions.
+
+(defn- sorted-group-by [sizes grouping-key]
+  (->> sizes
+       (group-by grouping-key)
+       (into (sorted-map))))
+
+(defn- format-partitions [topic-id partitions-per-topic]
+  (->> (sorted-group-by partitions-per-topic :partition)
+       (map-indexed (fn [idx [partition-name partition-details]]
+                      (let [partition-id (str idx)
+                            partition-size (reduce + (map :size partition-details))]
+                        {:id (str topic-id "." partition-id)
+                         :name partition-name
+                         :size partition-size
+                         :parent topic-id})))))
+
+(defn- transform-sizes [sizes]
+  (->> (sorted-group-by sizes :topic)
+       (map-indexed
+        (fn [idx [topic partitions]]
+          (let [topic-id (str idx)
+                topic-size (reduce + (map :size partitions))
+                parent-details {:id topic-id :name topic :size topic-size}
+                formatted-partitions (format-partitions topic-id partitions)]
+            (cons parent-details formatted-partitions))))
+       (apply concat)
+       vec))
 
 (defn categories-logical
   "Transform topic sizes into categorised logical view"
-  [sizes])
+  [sizes] (transform-sizes sizes))
 
 (defn categories-physical
   "Transform topic sizes into categorised physical view"
   [sizes])
+
